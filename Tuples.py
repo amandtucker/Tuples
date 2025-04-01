ef22ec8f05b6847715ffa96f8e13e69b35d4d102
amandtucker <88464804+amandtucker@users.noreply.github.com>
2025-04-01 09:16:59 +0530

    Add files via upload
    Need assistance in Index and count funnction

---
+++ Tuples.py
@@ -0,0 +1,19 @@
+# len() no of elements in tuple
+a=('10','30','20','40','40')
+print(len(a))
+# count()find the number of times an element is present 
+#print(count(a))
+# index() find index and occurane of element intuple raise exception if not found 
+#for x in ((a)):
+ #print (x)
+# sorted() takes tuple as input and returns Sorted list as output
+for x in (sorted(a)):
+ print (x)
+# # min()gives smallest element in tuple as an output
+for x in(min(a)):
+ print(x)
+# print(min(a))
+for x in(max(a)):
+ print(x)
+# # max() gives largest element in the tuple as an output 
+# print(max(a))
